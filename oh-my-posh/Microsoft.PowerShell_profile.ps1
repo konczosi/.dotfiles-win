9208032b097638ccd6d61c867a67b72ff383be02
Ignác Konczos <47603441+konczosi@users.noreply.github.com>
2023-08-28 21:07:46 +0200

    Create Microsoft.PowerShell_profile.ps1

---
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json"| Invoke-Expression
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#875f5f'}
+
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
