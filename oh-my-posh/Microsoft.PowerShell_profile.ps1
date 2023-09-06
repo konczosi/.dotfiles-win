232615cc280cbb091527bbf9865fa4776009b43a
Ignác Konczos <47603441+konczosi@users.noreply.github.com>
2023-09-06 23:58:36 +0200

    Update Microsoft.PowerShell_profile.ps1

--- oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,14 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json"| Invoke-Expression
-
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-$env:VIRTUAL_ENV_DISABLE_PROMPT = 1
-
 Import-Module PSReadLine
 
+Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource History
-
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-
 Set-PSReadLineOption -Colors @{ InlinePrediction = '#875f5f'}
 
-
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/konczosi.omp.json"| Invoke-Expression
+Enable-PoshTransientPrompt
