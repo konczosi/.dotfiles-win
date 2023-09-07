66b62cdf3cc43d5094b3d31262f4dfbc03d441a4
Ignác Konczos <47603441+konczosi@users.noreply.github.com>
2023-09-07 12:00:53 +0200

    Update Microsoft.PowerShell_profile.ps1

--- oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -5,15 +5,17 @@ Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineOption -Colors @{ InlinePrediction = '#875f5f'}
 
-Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord "Ctrl+p" -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord "Ctrl+n" -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord "Ctrl+LeftArrow" -Function BackwardWord
 
 Set-Alias cl "clear"
-function cdw { set-location "$workdir" }
-function cdd { set-location "$develdir" }
+
+function cdw { set-location "D:\Devel\PPT\" }
+function cdd { set-location "D:\Devel\" }
+
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/konczosi.omp.json"| Invoke-Expression
-Enable-PoshTransientPrompt
